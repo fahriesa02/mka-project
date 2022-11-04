2876939688830cd358c4dd33b254248f08639217
fahriesa02 <fujinkai02@gmail.com>
2022-11-04 17:23:55 +0800

    added 404 error handler on index.js

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ import { connection } from './config/db_config.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+
+app.use((req, res) => {
+    return res.status(404).json({
+        message: '404_NOT_FOUND'
+    });
+});
+
+
 connection();
 
 app.listen(3000, () => {
