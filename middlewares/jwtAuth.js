3f0dc5c11ff2b6799ea5ea9065c7c15de362e30d
fahriesa02 <fujinkai02@gmail.com>
2022-11-06 00:39:47 +0800

    bug with jwtauth middleware

--- middlewares/jwtAuth.js
+++ middlewares/jwtAuth.js
@@ -4,26 +4,19 @@ import dotenv from 'dotenv';
 const env = dotenv.config().parsed;
 
 const jwtAuth = () => {
-    return  function(req,res,next) {
+    return async (req,res,next) => {
         try {
-            if(!req.headers.authorization) {
-                const token = req.headers.authorization.split(' ')[1];
-                const verify = jwt.verify(token, env.ACCESS_TOKEN, function(error, data) {
-                    if(error) {
-                        if(error.name == 'TokenExpiredError') {
-                            throw 'TOKEN_EXPIRED'
-                        } else {
-                            throw 'TOKEN_IS_NOT_VALID'
-                        }
-                    } else {
-                        req.jwt = verify;
-                        next();
-                    }
-                });
-            } else {
-                throw 'TOKEN_REQUIRED'
+            if(!req.headers.authorization) throw {
+                code: 401,
+                message: 'UNAUTHORIZED_ACCESS'
             }
+            const token = req.headers.authorization.split(' ')[1];
+            console.log(token);
+            const verify = jwt.verify(token, env.ACCESS_TOKEN_SECRET);
+            console.log(verify);
+            req.jwt = verify;
 
+            next();
         } catch(error) {
             if(error.message == 'jwt expired') {
                 error.message = 'ACCESS_TOKEN_EXPIRED';
