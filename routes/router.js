c9c83b791584cd6949d0344c74a90ec4f2d24c69
fahriesa02 <fujinkai02@gmail.com>
2022-11-05 17:38:34 +0800

    modified router file

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Auth from '../controllers/UsersController.js';
+import Product from '../controllers/ProductsController.js';
 import jwtAuth from '../middlewares/jwtAuth.js';
 
 const router = express.Router();
@@ -16,7 +17,9 @@ router.post('/auth/login', Auth.Login);
 router.post('/auth/refresh-token', Auth.refreshToken);
 
 // PRODUCT endpoint
-
+router.get('/product/view_product', /*jwtAuth()*/ Product.getProducts);
+router.post('/product/input_product', /*jwtAuth()*/ Product.inputProduct);
+router.delete('/product/delete_product', /*jwtAuth()*/ Product.deleteProduct);
 
 
 export default router;
