b47243d7c021f3ad6e234908daad9327bffd40cd
fahriesa02 <fujinkai02@gmail.com>
2022-11-11 09:40:02 +0800

    added cors for different source request

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import { connection } from './config/db_config.js';
 import dotenv from 'dotenv';
 import router from './routes/router.js';
+import cors from 'cors';
 
 const app = express();
 const env = dotenv.config().parsed;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
