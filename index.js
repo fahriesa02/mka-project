4e662bd4eb9142d199d71e3e5dd5983e8d161795
fahriesa02 <fujinkai02@gmail.com>
2022-11-15 01:26:08 +0800

    Revert "added cors for different source request"
    This reverts commit b47243d7c021f3ad6e234908daad9327bffd40cd.

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ import express from 'express';
 import { connection } from './config/db_config.js';
 import dotenv from 'dotenv';
 import router from './routes/router.js';
-import cors from 'cors';
 
 const app = express();
 const env = dotenv.config().parsed;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
