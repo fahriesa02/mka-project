06651e48d6403ab08c776844cf2e99d29512881b
fahriesa02 <fujinkai02@gmail.com>
2022-11-21 14:19:31 +0800

    jwtauth

--- middlewares/jwtAuth.js
+++ middlewares/jwtAuth.js
@@ -4,32 +4,30 @@ import dotenv from 'dotenv';
 const env = dotenv.config().parsed;
 
 const jwtAuth = () => {
-    return async (req,res,next) => {
-        try {
-            if(!req.headers.authorization) throw {
-                code: 401,
-                message: 'UNAUTHORIZED_ACCESS'
-            }
-            const token = req.headers.authorization.split(' ')[1];
-            // console.log(token);
-            const verify = jwt.verify(token, env.ACCESS_TOKEN);
-            // console.log(verify);
-            req.jwt = verify;
+  return async (req, res, next) => {
+    try {
+      if(!req.headers.authorization) throw {
+        code: 401,
+        message: 'UNAUTHORIZED_ACCESS'
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      const verify = jwt.verify(token, env.ACCESS_TOKEN);
+      
+      req.jwt = verify;
 
-            next();
-        } catch(error) {
-            if(error.message == 'jwt expired') {
-                error.message = 'ACCESS_TOKEN_EXPIRED';
-            } else if(error.message == 'invalid signature' || error.message == 'jwt malformed' || error.message == 'jwt must be provided' || error.message == 'invalid token') {
-                error.message = 'INVALID_ACCESS_TOKEN'
-            }
-            console.log(error);
-            return res.status(error.code || 401).json({
-                status: false,
-                message: error.message
-            });
-        }
+      next();
+    } catch(error) {
+      if(error.message == 'jwt expired') {
+        error.message = 'ACCESS_TOKEN_EXPIRED';
+      } else if(error.message == 'invalid signature' || error.message == 'jwt malformed' || error.message == 'jwt must be provided' || error.message == 'invalid token') {
+        error.message = 'INVALID_ACCESS_TOKEN'
+      }
+      return res.status(error.code || 401).json({
+        status: false,
+        message: error.message
+      });
     }
+  }
 }
 
 export default jwtAuth;
