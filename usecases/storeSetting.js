3811ee5b51b0043604c12b6689badc7e840e01ed
fahriesa02 <fujinkai02@gmail.com>
2022-11-21 14:22:10 +0800

    new usecase for storesetting

---
+++ usecases/storeSetting.js
@@ -0,0 +1,14 @@
+import Stores from "../models/Stores.js";
+import getStoreProfile from "./getStoreProfile.js";
+
+export default async (params) => {
+  const storeProfile = await Stores.findOne({
+    where: {
+      id: params.jwt.storeId,
+    },
+  });
+  
+  if(!storeProfile) return [null, 'STORE_INFORMATION_IS_NOT_FOUND'];
+  
+  return [{ storeProfile }, null];
+}
