388466dfe61936c5fab7308c25f8877a9d14dbdc
fahriesa02 <fujinkai02@gmail.com>
2022-11-21 14:20:05 +0800

    added new route for profile and setting

--- routes/router.js
+++ routes/router.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import Auth from '../controllers/UsersController.js';
 import Product from '../controllers/ProductsController.js';
-import Profile from '../controllers/ProfileController.js'
+import Stores from '../controllers/ProfileController.js';
+import Settings from '../controllers/SettingController.js';
 import jwtAuth from '../middlewares/jwtAuth.js';
 
 const router = express.Router();
@@ -19,8 +20,11 @@ router.post('/auth/refresh-token', Auth.refreshToken);
 router.post('/auth/forget-password', Auth.forgetPassword);
 
 // PROFILE endpoint
-router.get('/profile', jwtAuth(), Profile.getProfile);
-router.put('/profile/edit_profile', jwtAuth(), Profile.updateProfile);
+router.get('/profile', jwtAuth(), Stores.getProfile);
+router.put('/profile/edit_profile', jwtAuth(), Stores.updateProfile);
+
+// SETTING endpoint
+router.get('/setting', jwtAuth(), Settings.getStoreSetting);
 
 // PRODUCT endpoint
 router.get('/product/view_product', jwtAuth(), Product.getProducts);
