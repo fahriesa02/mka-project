7ac2d4bb21a519f32acc917b8f05f843bac3d762
fahriesa02 <fujinkai02@gmail.com>
2022-11-21 14:22:33 +0800

    new use case for update store profile

---
+++ usecases/updateStoreProfile.js
@@ -0,0 +1,34 @@
+import Stores from "../models/Stores.js";
+
+export default async(params) => {  
+  const updateProfile = params.body;
+  const storeId = params.jwt.storeId;
+
+  const oldStoreProfile = await Stores.findOne({
+    where: {
+      id: storeId,
+    },
+  });
+  
+  const storeProfile = await Stores.update(updateProfile, {
+    where: {
+      id: storeId,
+    },
+  });
+  
+  if(!storeProfile) return [null, 'STORE_PROFILE_NOT_FOUND'];
+  
+  const updateStoreProfile = await Stores.findOne({
+    where: {
+      id: storeId,
+    },
+  });
+  
+  if(!updateStoreProfile) return [null, 'UPDATE_STORE_FAILURE'];
+
+
+  return [{
+    previousStoreProfile: oldStoreProfile,
+    updatedStoreProfile: updateStoreProfile,
+  }, null];
+}
